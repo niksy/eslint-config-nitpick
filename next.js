361fdec9134c5ddae8942290c89e0c1e79c966f9
Ivan Nikolić <ivan.nikolic@trikoder.net>
2017-05-23 15:53:37 +0200

    Adjust "next" configuration rules

--- next.js
+++ next.js
@@ -11,7 +11,7 @@ module.exports = {
 		'node/no-unsupported-features': 0,
 
 		// ES2015
-		'constructor-super': 0,
+		'constructor-super': 1,
 		'generator-star-spacing': [2, {
 			before: true,
 			after: false
@@ -19,19 +19,19 @@ module.exports = {
 		'no-class-assign': 1,
 		'no-dupe-class-members': 2,
 		'no-this-before-super': 2,
-		'object-shorthand': 0,
+		'object-shorthand': [1, 'consistent-as-needed'],
 		'prefer-reflect': 0,
-		'prefer-spread': 0,
+		'prefer-spread': 1,
 		'require-yield': 1,
 		'no-new-symbol': 2,
 		'no-restricted-imports': 0,
 		'no-useless-constructor': 1,
 		'prefer-rest-params': 1,
 		'sort-imports': 0,
-		'symbol-description': 0,
+		'symbol-description': 1,
 		'yield-star-spacing': [1, 'after'],
 		'no-duplicate-imports': 1,
-		'no-useless-computed-key': 0,
+		'no-useless-computed-key': 1,
 		'no-useless-rename': 1,
 		'rest-spread-spacing': [2, 'never'],
 		'class-methods-use-this': 0,
