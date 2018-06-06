b13dd2858c82178a579ba8b92a5090d2382b937a
Ivan Nikolić <niksy5@gmail.com>
2018-06-06 09:54:27 +0200

    Share JSX configuration between React and Vue

--- react.js
+++ react.js
@@ -2,11 +2,7 @@
 
 module.exports = {
 
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true
-		}
-	},
+	extends: require.resolve('./lib/jsx'),
 
 	plugins: [
 		'react'
@@ -30,7 +26,6 @@ module.exports = {
 		'react/forbid-elements': 0,
 		'react/forbid-prop-types': 0,
 		'react/forbid-foreign-prop-types': 0,
-		'react/no-array-index-key': 2,
 		'react/no-children-prop': 1,
 		'react/no-danger': 2,
 		'react/no-danger-with-children': 2,
@@ -45,7 +40,6 @@ module.exports = {
 		'react/no-render-return-value': 1,
 		'react/no-set-state': 0,
 		'react/no-string-refs': 2,
-		'react/no-unescaped-entities': 2,
 		'react/no-unknown-property': 0,
 		'react/no-unused-prop-types': 0,
 		'react/prefer-es6-class': 0,
@@ -54,57 +48,8 @@ module.exports = {
 		'react/react-in-jsx-scope': 2,
 		'react/require-default-props': 0,
 		'react/require-optimization': 0,
-		'react/require-render-return': 2,
-		'react/self-closing-comp': 1,
 		'react/sort-comp': 1,
 		'react/sort-prop-types': 0,
-		'react/style-prop-object': 1,
-		'react/void-dom-elements-no-children': 2,
-
-		// JSX
-		'react/jsx-space-before-closing': 0,
-		'react/jsx-child-element-spacing': 1,
-		'react/jsx-closing-tag-location': 2,
-		'react/jsx-curly-brace-presence': [1, 'never'],
-		'react/jsx-max-depth': [1, { max: 15 }],
-		'react/jsx-one-expression-per-line': 0,
-		'react/jsx-props-no-multi-spaces': 2,
-		'react/button-has-type': 2,
-		'react/jsx-boolean-value': [2, 'always'],
-		'react/jsx-closing-bracket-location': [1, 'tag-aligned'],
-		'react/jsx-curly-spacing': [1, 'never'],
-		'react/jsx-equals-spacing': [1, 'never'],
-		'react/jsx-filename-extension': 0,
-		'react/jsx-first-prop-new-line': 0,
-		'react/jsx-handler-names': 1,
-		'react/jsx-indent': [2, 'tab'],
-		'react/jsx-indent-props': [2, 'tab'],
-		'react/jsx-key': 2,
-		'react/jsx-max-props-per-line': [1, { maximum: 5 }],
-		'react/jsx-no-bind': [1, {
-			ignoreRefs: true,
-			allowArrowFunctions: true
-		}],
-		'react/jsx-no-comment-textnodes': 1,
-		'react/jsx-no-duplicate-props': 1,
-		'react/jsx-no-literals': 0,
-		'react/jsx-no-target-blank': 1,
-		'react/jsx-no-undef': 1,
-		'react/jsx-pascal-case': 2,
-		'react/jsx-sort-props': [1, {
-			ignoreCase: true,
-			callbacksLast: true,
-			shorthandFirst: true,
-			noSortAlphabetically: true,
-			reservedFirst: true
-		}],
-		'react/jsx-tag-spacing': [1, {
-			closingSlash: 'never',
-			beforeSelfClosing: 'always',
-			afterOpening: 'never'
-		}],
-		'react/jsx-uses-react': 1,
-		'react/jsx-uses-vars': 1,
-		'react/jsx-wrap-multilines': 2
+		'react/jsx-uses-react': 1
 	}
 };
--- vue.js
+++ vue.js
@@ -2,15 +2,7 @@
 
 module.exports = {
 
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true
-		}
-	},
-
-	plugins: [
-		'react'
-	],
+	extends: require.resolve('./lib/jsx'),
 
 	rules: {
 
@@ -19,59 +11,6 @@ module.exports = {
 		'no-param-reassign': [1, {
 			props: true,
 			ignorePropertyModificationsFor: ['state']
-		}],
-
-		// React/JSX
-		'react/no-array-index-key': 2,
-		'react/no-unescaped-entities': 2,
-		'react/require-render-return': 2,
-		'react/self-closing-comp': 1,
-		'react/style-prop-object': 1,
-		'react/void-dom-elements-no-children': 2,
-
-		// JSX
-		'react/jsx-space-before-closing': 0,
-		'react/jsx-child-element-spacing': 1,
-		'react/jsx-closing-tag-location': 2,
-		'react/jsx-curly-brace-presence': [1, 'never'],
-		'react/jsx-max-depth': [1, { max: 15 }],
-		'react/jsx-one-expression-per-line': 0,
-		'react/jsx-props-no-multi-spaces': 2,
-		'react/button-has-type': 2,
-		'react/jsx-boolean-value': [2, 'always'],
-		'react/jsx-closing-bracket-location': [1, 'tag-aligned'],
-		'react/jsx-curly-spacing': [1, 'never'],
-		'react/jsx-equals-spacing': [1, 'never'],
-		'react/jsx-filename-extension': 0,
-		'react/jsx-first-prop-new-line': 0,
-		'react/jsx-handler-names': 1,
-		'react/jsx-indent': [2, 'tab'],
-		'react/jsx-indent-props': [2, 'tab'],
-		'react/jsx-key': 2,
-		'react/jsx-max-props-per-line': [1, { maximum: 5 }],
-		'react/jsx-no-bind': [1, {
-			ignoreRefs: true,
-			allowArrowFunctions: true
-		}],
-		'react/jsx-no-comment-textnodes': 1,
-		'react/jsx-no-duplicate-props': 1,
-		'react/jsx-no-literals': 0,
-		'react/jsx-no-target-blank': 1,
-		'react/jsx-no-undef': 1,
-		'react/jsx-pascal-case': 2,
-		'react/jsx-sort-props': [1, {
-			ignoreCase: true,
-			callbacksLast: true,
-			shorthandFirst: true,
-			noSortAlphabetically: true,
-			reservedFirst: true
-		}],
-		'react/jsx-tag-spacing': [1, {
-			closingSlash: 'never',
-			beforeSelfClosing: 'always',
-			afterOpening: 'never'
-		}],
-		'react/jsx-uses-vars': 1,
-		'react/jsx-wrap-multilines': 2
+		}]
 	}
 };
