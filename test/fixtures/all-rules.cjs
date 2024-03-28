cad7c98f05ac14eb64c6690da53df5638c1cf817
Ivan Nikolić <niksy5@gmail.com>
2024-03-28 14:17:22 +0100

    Incremental switch to ESM

--- test/fixtures/all-rules.cjs
+++ test/fixtures/all-rules.cjs
@@ -4,14 +4,14 @@ const path = require('path');
 
 module.exports = {
 	'extends': [
-		'./',
-		'./browser',
-		'./next',
-		'./tests',
-		'./react',
-		'./jsx',
-		'./vue',
-		'./typescript'
+		'./index.js',
+		'./browser.js',
+		'./next.js',
+		'./tests.js',
+		'./react.js',
+		'./jsx.js',
+		'./vue.js',
+		'./typescript.js'
 	].map((m) => {
 		return require.resolve(path.resolve(process.cwd(), m));
 	})
