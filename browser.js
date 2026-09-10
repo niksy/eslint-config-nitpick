a54cf6c4b50476651053f3f6feac93730eca50e5
Ivan Nikolić <niksy5@gmail.com>
2026-09-10 15:24:53 +0200

    Remove deprecated rules

--- browser.js
+++ browser.js
@@ -23,7 +23,6 @@ export default defineConfig({
 		'unicorn/require-post-message-target-origin': 2,
 		'unicorn/prefer-dom-node-append': 0,
 		'unicorn/prefer-dom-node-remove': 0,
-		'unicorn/prefer-dom-node-dataset': 1,
 		'unicorn/dom-node-dataset': 1,
 		'unicorn/prefer-dom-node-text-content': 1,
 		'unicorn/prefer-modern-dom-apis': 1,
